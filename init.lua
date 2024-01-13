6ebd07bc946d11e6a4887c241264b132321ef773
Ravshan Samandarov <ravshansbox@gmail.com>
2024-01-13 19:16:25 +0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,43 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.showmode = false
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.cmd("colorscheme shine")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-lualine/lualine.nvim",
+    config = true,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    name = 'nightfox',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nightfox').setup()
+      vim.cmd('colorscheme dayfox')
+    end,
+  },
+  { "github/copilot.vim" },
+})
+
