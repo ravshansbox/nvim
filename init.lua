135cdefe108ee662b5bf41c5a28e0ec3b9234e56
Ravshan Samandarov <ravshansbox@gmail.com>
2024-01-13 19:16:25 +0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,40 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.mouse = ""
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.showmode = false
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('github-theme').setup()
+      vim.cmd('colorscheme github_light_default')
+    end,
+  },
+  { "nvim-lualine/lualine.nvim", config = true },
+})
+
